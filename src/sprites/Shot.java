b686dc1c8b9a32b8bd2ea4e6bc704dd8ca9f2e48
Alexander Mitic <miticalexander@yahoo.com>
2018-04-27 05:35:54 +0200

    - shot appearance improved
    - Shot is a rotating red diamond, instead of a red circle

--- src/sprites/Shot.java
+++ src/sprites/Shot.java
@@ -2,16 +2,19 @@ package sprites;
 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
 
 public class Shot extends Sprite {
-
     private static final double VELOCITY = -5;
-    private static final int RADIUS = 3;
+    private static final double ROT_VELOCITY = 5;
+    //private static final int RADIUS = 3;
     
-    private Circle body;
+    //private Circle body;
+    private Polygon body;
     
     public Shot() {
-        body = new Circle(0, 0, RADIUS);
+        //body = new Circle(0, 0, RADIUS);
+        body = new Polygon(0,8, -5,3, -5,-3, 0,-8, 5,-3, 5,3);
         body.setFill(Color.RED);
         getChildren().addAll(body);
     }
@@ -19,6 +22,7 @@ public class Shot extends Sprite {
     @Override
     public void update() {
         setTranslateY(getTranslateY() + VELOCITY);
+        setRotate(getRotate() + ROT_VELOCITY);
     }
     
 }
